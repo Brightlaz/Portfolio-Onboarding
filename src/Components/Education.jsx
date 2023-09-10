3260f1e84c9e896f79e2e44571d75ddea7aa506c
Bright Lazarus <lazbright1@gmail.com>
2023-09-10 10:46:03 +0000

    Made use of copilot

---
+++ src/Components/Education.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <div className="container">
+                <div className="box">
+                    <h3>University of Lagos</h3>
+                    <p className="small">BSc. Computer Science</p>
+                    <p className="small">2017 - 2021</p>
+                </div>
+                <div className="box">
+                    <h3>University of Lagos</h3>
+                    <p className="small">BSc. Computer Science</p>
+                    <p className="small">2017 - 2021</p>
+                </div>
+                <div className="box">
+                    <h3>University of Lagos</h3>
+                    <p className="small">BSc. Computer Science</p>
+                    <p className="small">2017 - 2021</p>
+                </div>
+                <div className="box">
+                    <h3>University of Lagos</h3>
+                    <p className="small">BSc. Computer Science</p>
+                    <p className="small">2017 - 2021</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Education;
